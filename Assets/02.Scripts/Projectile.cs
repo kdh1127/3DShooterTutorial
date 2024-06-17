0034f6e9e65ae2d400ffb0b54f07cca2c6ae5f9f
kdh1127 <kdkdhh1127@gmail.com>
2024-06-17 20:32:51 +0900

    E03. enemies

--- Assets/02.Scripts/Projectile.cs
+++ Assets/02.Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
 
     public void SetSpeed(float newSpeed)
@@ -12,6 +13,25 @@ public class Projectile : MonoBehaviour
     }
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray,out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
     }
 }
