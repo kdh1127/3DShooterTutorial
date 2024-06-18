94b703ac046c15b39393d66cc92ab6e5ed82a59b
kdh1127 <kdkdhh1127@gmail.com>
2024-06-18 21:01:57 +0900

    E09. obstacle placement

---
+++ Assets/02.Scripts/Utility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+public static class Utility
+{
+    public static T[] ShuffleArray<T>(T[] array, int seed)
+    {
+        System.Random prng = new System.Random(seed);
+
+        for (int i = 0; i < array.Length -1; i++)
+        {
+            int randomIndex = prng.Next(i, array.Length);
+            T trmpItem = array[randomIndex];
+            array[randomIndex] = array[i];
+            array[i] = trmpItem;
+        }
+        return array;
+    }
+}
